550c29624b1087737e7d535fccc11172e1485607
yangyang11412 <yangyang11412@autohome.com.cn>
2022-10-16 19:56:09 +0800

    测试响应式字段

--- src/views/table/table.vue
+++ src/views/table/table.vue
@@ -1,4 +1,7 @@
 <template>
+    <el-button @click="changeTitle">更改title</el-button>
+    <el-button @click="changeArr">更改arr</el-button>
+    <h1>{{title}}</h1>
     <el-table :data="tableData" style="width: 100%">
         <el-table-column prop="date" label="Date" width="180" />
         <el-table-column prop="name" label="Name" width="180" />
@@ -7,7 +10,14 @@
 </template>
 
 <script setup>
-const tableData = [
+import {onMounted, ref, reactive} from 'vue';
+
+let title = '测试表格'
+function changeTitle() {
+    title = '新的标题'
+}
+
+const tableData = reactive([
     {
         date: '2016-05-03',
         name: 'Tom',
@@ -28,7 +38,19 @@ const tableData = [
         name: 'Tom',
         address: 'No. 189, Grove St, Los Angeles',
     },
-]
+])
+
+function changeArr() {
+    tableData.shift();
+}
+
+onMounted(() => {
+    tableData.push({
+        date: '2022-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    })
+})
 </script>
 
 <style scoped>
