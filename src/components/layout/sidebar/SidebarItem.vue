e3ceaab315211e0c65952eb17cfb72843d83406c
yangyang11412 <yangyang11412@autohome.com.cn>
2022-11-03 21:02:46 +0800

    菜单路由联动

--- src/components/layout/sidebar/SidebarItem.vue
+++ src/components/layout/sidebar/SidebarItem.vue
@@ -1,13 +1,14 @@
 <template>
     <template v-if="!routeItem.meta?.hideMenu">
-        <el-menu-item v-if="showMenuComponent"
-                      :index="resolvePath">
+        <!-- 展示菜单 -->
+        <el-menu-item v-if="isMenu" :index="resolvePath">
             <el-icon>
                 <icon-menu/>
             </el-icon>
             <span>{{ routeItem.meta?.title }}</span>
         </el-menu-item>
 
+        <!-- 展示目录 -->
         <el-sub-menu v-else :index="routeItem.path">
             <template #title>
                 <el-icon>
@@ -16,7 +17,7 @@
                 <span>{{ routeItem.meta?.title }}</span>
             </template>
             <sidebar-item
-                v-for="child in routeItem.children"
+                v-for="child of routeItem.children"
                 :key="child.path"
                 :route-item="child"
                 :base-path="resolvePath"
@@ -26,35 +27,33 @@
 </template>
 
 <script setup>
+import {computed} from "vue"
 import path from 'path-browserify';
-import {computed, reactive, ref} from "vue"
 import {Location, Menu as IconMenu} from "@element-plus/icons-vue";
 
 const props = defineProps(['routeItem', 'basePath']);
-let routeItem = reactive(props.routeItem)
-let basePath = ref(props.basePath);
 
-let children = routeItem.children || []
-console.log(reactive([]))
-const displayedChildrenLength = computed(() => children.filter(item => !item.meta?.hideMenu).length)
-const test = computed(() => 1 + 1)
-console.log(test)
+/*
+ * 当前节点的子节点数量
+ * 0: 此节点即为菜单节点
+ * 1: 只有一个child，则父目录节点替换为该child的路由配置；
+ */
+let children = props.routeItem.children || []
+const childAmount = computed(() => children.filter(item => !item.meta?.hideMenu).length)
 
-// 展示菜单组件（而不是目录）
-const showMenuComponent = computed(() => {
-    // 不一直展示目录，且，children数量 <= 1（等于1：只有一个child，则父目录节点替换为该child的路由配置；小于1：此节点即为菜单节点
-    return !routeItem.meta?.alwaysShowDirectory && children.filter(item => !item.meta?.hideMenu).length <= 1;
+// 当前节点是否是菜单组件（而不是目录）
+const isMenu = computed(() => {
+    return !props.routeItem.meta?.alwaysShowDirectory && childAmount.value <= 1;
 })
 
+// 当前节点的路径处理
 let resolvePath = computed(() => {
-    if (!routeItem.meta?.alwaysShowDirectory) {
-        if (children.filter(item => !item.meta?.hideMenu).length === 1) {
-            return path.resolve(basePath.value, children[0].path);
+    if (!props.routeItem.meta?.alwaysShowDirectory) {
+        if (childAmount.value === 1) {
+            // 只有一个child
+            return path.resolve(props.basePath, children[0].path);
         }
     }
-    return path.resolve(basePath.value, routeItem.path);
+    return path.resolve(props.basePath, props.routeItem.path);
 })
-
-
-
 </script>
